bf68ace452d0ae368a86a7f7e1f237782fc790e4
saif <s4saif.121@gmail.com>
2020-06-19 22:54:47 +0400

    init

---
+++ temperature-edge-app/index.js
@@ -0,0 +1,26 @@
+const { Node } = require("flatend");
+const Database = require("better-sqlite3");
+
+const db = new Database(":memory:");
+db.exec(
+  `CREATE TABLE temperature (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    , temperature INTEGER)`
+);
+
+const all = async (ctx) =>ctx.json(await db.prepare(`SELECT id, timestamp, temperature FROM temperature`).all());
+const interval = setInterval(function() {
+  const temp= Math.random() * (30 - 20) + 20
+  db.exec(
+    `INSERT INTO temperature (temperature) VALUES( ${temp})`
+  );
+}, 5000);
+const main = async () => {
+  await Node.start({
+    addrs: ["127.0.0.1:9000"],
+    services: {
+      all_temperature: all,
+    },
+  });
+};
+
+main().catch((err) => console.error(err));
